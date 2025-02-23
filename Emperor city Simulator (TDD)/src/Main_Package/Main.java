ccb8f447cf19dd07295187231b9b20be726d8ff8
danybonfi88 <bonfstef2304@gmail.com>
2025-02-23 20:47:52 +0100

    Ultimo Commit: Main + Gestione delle Eccezioni

--- Emperor city Simulator (TDD)/src/Main_Package/Main.java
+++ Emperor city Simulator (TDD)/src/Main_Package/Main.java
@@ -2,7 +2,80 @@ package Main_Package;
 
 public class Main {
     public static void main(String[] args) {
+        Citta citta1 = new Citta();
 
+        boolean cond1 = true, cond2 = true, cond3 = true, cond4 = true, cond5 = true, cond6 = true;
+        int x, y, z, w, probRivolta, probInvasione;
+
+
+        // PRODUZIONE GRANAIO //
+        x = citta1.granaioProduciRisorse();
+        try{
+            checkRisorseMax(citta1, x);
+        } catch (MassimoRisorseException e) {
+            System.out.println("Errore: la produzione è stata scartata" + e.getMessage());
+            cond1=false;
+        }
+        if(cond1){citta1.granaioAumRisorse(x);}
+
+
+        // INVIO PORTO //
+        y = citta1.portoInviaRisorse();
+        try{
+            checkRisorseInvio(citta1, y);
+        } catch (ScarsitaRisorseException e) {
+            System.out.println("Errore: " + e.getMessage());
+            cond2=false;
+        }
+        if(cond2){citta1.granaioDimRisorse(y);}
+
+
+        // RICEVI PORTO //
+        z = citta1.portoRiceviRisorse();
+        try{
+            checkRisorseMax(citta1, z);
+        } catch (MassimoRisorseException e) {
+            System.out.println("Errore: " + e.getMessage());
+            cond3=false;
+        }
+        if(cond3){citta1.granaioAumRisorse(z);}
+
+
+        // ADDESTRAMENTO CASERMA //
+        w = citta1.casermaAddestraSoldati() * 5;
+        try{
+            checkAddestraSoldati(citta1, w);
+        } catch(AddestramentoFallitoException e){
+            System.out.println("Errore: " + e.getMessage());
+            cond4=false;
+        }
+        if(cond4){citta1.casermaAumSoldati(w); citta1.granaioDimRisorse(w*5);}
+
+
+        // RIVOLTA //
+        probRivolta = (int)(Math.random()*(10-1))+1;
+        if(probRivolta <= 4){
+            try{
+                checkSoldatiRivolta(citta1);
+            } catch(RivoltaAvvenutaException e){
+                System.out.println("Errore: " + e.getMessage());
+                cond5=false;
+            }
+            if(!cond5){citta1.granaioZeroRisorse(); citta1.casermaDimSoldati(2);}
+        }
+
+
+        // INVASIONE //
+        probInvasione = (int)(Math.random()*(10-1))+1;
+        if(probInvasione <= 2){
+            try{
+                checkSoldatiInvasione(citta1);
+            } catch(InvasioneAvvenutaException e){
+                System.out.println("Errore: " + e.getMessage());
+                cond6=false;
+            }
+            if(!cond6){citta1.granaioZeroRisorse(); citta1.casermaDimSoldati(0);}
+        }
     }
 
     public static void checkRisorseMax(Citta citta, int n)throws MassimoRisorseException{
